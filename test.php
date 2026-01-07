baa7747bc96356e5b5e9d805a7f09235c7a14405
jashimUddin1 <72594127+jashimUddin1@users.noreply.github.com>
2026-01-07 22:08:17 +0600

    Clean up test.php by removing comments
    Removed placeholder comments and notes from test.php

--- test.php
+++ test.php
@@ -1,8 +1,1 @@
-ami chassi ai serial a dekhate 
 
-first school thakbe (create, update, delete)
-2nd note ..
-3rd invoice
-4th accounts  
-
-aivabe kore dekhabe . mani jodi invoice dekhay tobe invoice create then update then delete.
